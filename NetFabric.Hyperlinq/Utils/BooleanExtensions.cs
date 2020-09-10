1a3af23c23f091c1f45c156e6eded83b622a9076
mphelt <25581948+mphelt@users.noreply.github.com>
2020-09-10 20:31:22 +0200

    Update BooleanExtensions.cs

--- NetFabric.Hyperlinq/Utils/BooleanExtensions.cs
+++ NetFabric.Hyperlinq/Utils/BooleanExtensions.cs
@@ -10,11 +10,12 @@ namespace NetFabric.Hyperlinq
         public static byte AsByte(this bool value)
             => Unsafe.As<bool, byte>(ref value);
 
+        // condition ? consequent : alternative
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int Conditional(this bool condition, int consequent, int alternative)
         {
-            var mask = -condition.AsByte() >> 31; // test ? 1..1 : 0..0
-            return (mask & consequent) + (~mask & alternative); // test ? end : index == (test ? end : 0) + (test ? 0 : index)
+            var mask = -condition.AsByte() >> 31; // condition ? 0b1..1 : 0b0..0
+            return (mask & consequent) + (~mask & alternative); // (condition ? consequent : 0) + (condition ? 0 : alternative)
         }
     }
 }
